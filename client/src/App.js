3e749ab1a585c3c6533e4a3b7b4a5e9ec3ae0d8c
phil <philgoodmusic@gmail.com>
2018-10-06 12:46:42 -0400

    Adding app structure

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Chat from "./pages/Chat";
+import ChatRoom from "./pages/ChatRoom";
+import ChatRoom2 from "./pages/ChatRoom2";
+import ChatRoom3 from "./pages/ChatRoom3";
+import ChatRoom4 from "./pages/ChatRoom4";
+import ChatRoom5 from "./pages/ChatRoom5";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+
+const App = () => (
+  <Router>
+    <div>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={Chat} />
+        <Route exact path="/books" component={Chat} />
+        <Route exact path="/chat/1" component={ChatRoom} />
+        <Route exact path="/chat/2" component={ChatRoom2} />
+        <Route exact path="/chat/3" component={ChatRoom3} />
+        <Route exact path="/chat/4" component={ChatRoom4} />
+        <Route exact path="/chat/5" component={ChatRoom5} />
+        <Route component={NoMatch} />
+      </Switch>
+    </div>
+  </Router>
+);
+
+export default App;
